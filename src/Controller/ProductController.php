86b7a195eb98f1b1d70bead656eb37eaa4bce78a
Constance Laloux <constancelaloux@gmail.com>
2023-05-18 00:55:31 +0200

    I have created a product form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,16 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/{slug}', name: 'app_category')]
-    public function category($slug, CategoryRepository $categoryRepository): Response
+    /**
+     * @Route("/{slug}", name="product_category")
+     */
+    public function category(string $slug, CategoryRepository $categoryRepository): Response
     {
         $category = $categoryRepository->findOneBy(['slug' => $slug]);
 
@@ -24,8 +32,10 @@ class ProductController extends AbstractController
         ]);
     }
 
-    #[Route('/{category_slug}/{slug}', name: 'product_show')]
-    public function show($slug, ProductRepository $productRepository)
+    /**
+     * @Route("/{category_slug}/{slug}", name="product_show")
+     */
+    public function show(string $slug, ProductRepository $productRepository): Response
     {
         $product = $productRepository->findOneBy(['slug' => $slug]);
 
@@ -33,5 +43,40 @@ class ProductController extends AbstractController
             throw $this->createNotFoundException("Le produit demandé n'existe pas");
         }
         return ($this->render('product/show.html.twig', ['product' => $product]));
+    }
+
+    /**
+     * @Route("/admin/product/create", name="product_create")
+     */
+    public function create(FormFactoryInterface $factory): Response
+    {
+        $builder = $factory->createBuilder();
+
+        $builder->add('name', TextType::class, [
+                    'label' => 'Nom du produit',  
+                    'attr' => ['placeholder' => 'Tapez le nom du produit']])
+                ->add('shortDescription', TextareaType::class, [
+                    'label' => 'Description courte', 
+                    'attr' => [
+                        'placeholder' => 'Tapez une description assez courte mais parlante pour le visiteur']])
+                ->add('price', MoneyType::class, [
+                    'label' => 'Prix du produit',  
+                    'attr' => [
+                        'placeholder' => 'Prix du produit en €']])
+                ->add('category', EntityType::class, [
+                    'label' => 'Catégorie',  
+                    'placeholder' => '-- Choisir une catégorie --',
+                    'class' => Category::class,
+                    'choice_label' => function(Category $category){
+                        return strtoupper($category->getName());
+                    }
+                ]);
+
+        $form = $builder->getForm();
+        $formView = $form->createView();
+
+        return $this->render('product/create.html.twig', [
+            'formView' => $formView 
+        ]);
     }
 }
