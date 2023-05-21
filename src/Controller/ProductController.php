8080814c3fc82addcdeaa01f1652926c2de3362d
Constance Laloux <constancelaloux@gmail.com>
2023-05-22 00:59:22 +0200

    push after pull

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,32 +2,16 @@
 
 namespace App\Controller;
 
-<<<<<<< HEAD
-use App\Entity\Category;
-use App\Repository\CategoryRepository;
-use App\Repository\ProductRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-=======
-use App\Entity\Product;
 use App\Form\ProductType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
->>>>>>> 6b9c5b6 (i ve created a form to create products)
 
 class ProductController extends AbstractController
 {
@@ -46,21 +30,14 @@ class ProductController extends AbstractController
             'category' => $category,
         ]);
     }
-
+    
     /**
-<<<<<<< HEAD
-     * @Route("/{category_slug}/{slug}", name="product_show")
-=======
      * @Route("/category/{category_slug}/{slug}", name="product_show")
->>>>>>> 6b9c5b6 (i ve created a form to create products)
      */
     public function show(string $slug, ProductRepository $productRepository): Response
     {
         $product = $productRepository->findOneBy(['slug' => $slug]);
-<<<<<<< HEAD
 
-=======
->>>>>>> 6b9c5b6 (i ve created a form to create products)
         if (!$product) {
             throw $this->createNotFoundException("Le produit demandé n'existe pas");
         }
@@ -70,33 +47,6 @@ class ProductController extends AbstractController
     /**
      * @Route("/admin/product/create", name="product_create")
      */
-<<<<<<< HEAD
-    public function create(FormFactoryInterface $factory): Response
-    {
-        $builder = $factory->createBuilder();
-
-        $builder->add('name', TextType::class, [
-                    'label' => 'Nom du produit',  
-                    'attr' => ['placeholder' => 'Tapez le nom du produit']])
-                ->add('shortDescription', TextareaType::class, [
-                    'label' => 'Description courte', 
-                    'attr' => [
-                        'placeholder' => 'Tapez une description assez courte mais parlante pour le visiteur']])
-                ->add('price', MoneyType::class, [
-                    'label' => 'Prix du produit',  
-                    'attr' => [
-                        'placeholder' => 'Prix du produit en €']])
-                ->add('category', EntityType::class, [
-                    'label' => 'Catégorie',  
-                    'placeholder' => '-- Choisir une catégorie --',
-                    'class' => Category::class,
-                    'choice_label' => function(Category $category){
-                        return strtoupper($category->getName());
-                    }
-                ]);
-
-        $form = $builder->getForm();
-=======
     public function create(FormFactoryInterface $factory, Request $request, SluggerInterface $slugger, EntityManagerInterface $em): Response
     {
         $form = $this->createForm(ProductType::class);
@@ -110,7 +60,7 @@ class ProductController extends AbstractController
             $em->persist($product);
             $em->flush();
         }
->>>>>>> 6b9c5b6 (i ve created a form to create products)
+        
         $formView = $form->createView();
 
         return $this->render('product/create.html.twig', [
